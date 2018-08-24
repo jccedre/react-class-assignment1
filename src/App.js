b6f7c1e8e640cb189ef8fc5a4fbc9f07a0687db6
Jean <jccedre@gmail.com>
2018-08-24 15:26:20 -0400

    Completed Assignment 1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled, { ThemeProvider } from 'styled-components';
+import { main } from './Styles/Main';
+import UserOutput from './UserOutput/UserOutput';
+import UserInput from './UserInput/UserInput';
 
+const StyledApp = styled.section`
+  margin: 1em auto;
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
+  max-width: 75em;
+  padding: 1em;
+`
 class App extends Component {
+
+  state = {
+    username: 'Jean',
+  }
+
+  usernameChangeHandler = (event) => {
+    this.setState( {
+      username: event.target.value,
+    })
+
+  }
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <ThemeProvider theme={main}>
+        <StyledApp>
+          <UserInput username={this.state.username} changed={this.usernameChangeHandler}></UserInput>
+          <UserOutput username={this.state.username}></UserOutput>
+          <UserOutput></UserOutput>
+        </StyledApp>
+      </ThemeProvider>
     );
   }
 }
---
+++ src/UserOutput/UserOutput.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const userOutput = (props) => {
+  return (
+    <div>
+      <p>{props.username + ' '}
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam finibus semper fermentum. Nulla tempor rhoncus eros, ut rutrum velit tempor quis. Integer suscipit suscipit velit, at pharetra quam mattis in. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer sagittis at libero et pretium. Quisque commodo nisi interdum elementum finibus. Donec vel lacus laoreet, pulvinar dui non, varius turpis. Integer tincidunt ipsum ante, vitae blandit velit vehicula quis. Fusce imperdiet dui non mattis condimentum.
+      </p>
+      <p>
+        Praesent nec accumsan elit, eu volutpat nisi. Aliquam erat volutpat. Phasellus pellentesque consectetur massa, a ultrices turpis venenatis vitae. Nulla et euismod ex. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum ut odio at massa laoreet fermentum vitae in orci. Nunc vel rhoncus nisl. Aliquam at nibh vel lorem molestie semper ut vitae dui. Praesent non est tristique, consectetur turpis in, lobortis justo.
+      </p>
+    </div>
+  )
+};
+
+export default userOutput;
